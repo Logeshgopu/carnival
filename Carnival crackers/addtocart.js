1a8a7142641baa5286a23013329180d9a6c3cef6
LOGESH.G <128713913+Logeshgopu@users.noreply.github.com>
2024-07-09 15:15:13 +0530

    Add files via upload

---
+++ Carnival crackers/addtocart.js
@@ -0,0 +1,171 @@
+const product = [
+    {
+        id: 0,
+        image: 'image/gg-1.jpg',
+        title: 'Z Flip Foldable Mobile',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: 'image/hh-2.jpg',
+        title: 'Air Pods Pro',
+        price: 60,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 3,
+        image: 'image/aa-1.jpg',
+        title: 'Head Phones',
+        price: 100,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 1,
+        image: 'image/hh-2.jpg',
+        title: 'Air Pods Pro',
+        price: 60,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 3,
+        image: 'image/aa-1.jpg',
+        title: 'Head Phones',
+        price: 100,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 3,
+        image: 'image/aa-1.jpg',
+        title: 'Head Phones',
+        price: 100,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    },
+    {
+        id: 1,
+        image: 'image/hh-2.jpg',
+        title: 'Air Pods Pro',
+        price: 60,
+    },
+    {
+        id: 2,
+        image: 'image/ee-3.jpg',
+        title: '250D DSLR Camera',
+        price: 230,
+    }
+    
+];
+
+const categories = [...new Set(product.map((item) => item))];
+let i = 0;
+
+document.getElementById('root').innerHTML = categories.map((item) => {
+    const { image, title, price } = item;
+    return (
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image} alt='${title}'>
+            </div>
+            <div class='bottom'>
+                <p>${title}</p>
+                <h2>$ ${price}.00</h2>
+                <button onclick='addtocart(${i++})'>Add to cart</button>
+            </div>
+        </div>`
+    );
+}).join('');
+
+let cart = [];
+
+function addtocart(a) {
+    cart.push({ ...categories[a] });
+    displaycart();
+}
+
+function delElement(a) {
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart() {
+    let j = 0, total = 0;
+    document.getElementById("count").innerHTML = cart.length;
+    if (cart.length === 0) {
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ 0.00";
+    } else {
+        document.getElementById('cartItem').innerHTML = cart.map((items) => {
+            const { image, title, price } = items;
+            total += price;
+            document.getElementById("total").innerHTML = "$ " + total + ".00";
+            return (
+                `<div class='cart-item'>
+                    <div class='row-img'>
+                        <img class='rowimg' src=${image} alt='${title}'>
+                    </div>
+                    <p style='font-size:12px;'>${title}</p>
+                    <h2 style='font-size: 15px;'>$ ${price}.00</h2>
+                    <i class='fa-solid fa-trash' onclick='delElement(${j++})'></i>
+                </div>`
+            );
+        }).join('');
+    }
+}
+
+function toggleCart() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.display === 'none' || sidebar.style.display === '') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+}
+
+function orderItems() {
+    alert('Order placed successfully!');
+    cart = [];
+    displaycart();
+    toggleCart();
+}
